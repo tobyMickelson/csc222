ff24c962e27b881ffcb45f82b56009f37b8eb6f1
Toby Mickelson <tmickelson@tinyfern.com>
2024-11-12 10:52:56 -0500

    pass all tests

--- render_vector_test/num_vector_utils.cpp
+++ render_vector_test/num_vector_utils.cpp
@@ -30,6 +30,13 @@ int num_vector_product(const vector<int>& numbers) {
 
     return product;
 }
-vector<int> only_evens(const vector<int>&) {
-    return vector<int> {};
+
+vector<int> only_evens(const vector<int>& numbers) {
+    vector<int> output = {};
+
+    for (int number : numbers)
+        if (number % 2 == 0)
+            output.push_back(number);
+
+    return output;
 }

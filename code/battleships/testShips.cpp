db2865186849c6b613acf2de08e113dbd3e6339e
Alessandra <alessandra@casamendoza.net>
2024-12-05 08:11:32 -0500

    rotation works

--- code/battleships/testShips.cpp
+++ code/battleships/testShips.cpp
@@ -10,7 +10,12 @@ TEST_CASE("No bombs away") {
 TEST_CASE("Yes bombs away") {
     CHECK(Ship(4, 5, 6, false).explode(6, 5) == true);
 }
-
+TEST_CASE("Yes (vertical) bombs away") {
+    CHECK(Ship(4, 5, 3, true).explode(4, 7) == true);
+}
+TEST_CASE("No (vertical) bombs away") {
+    CHECK(Ship(4, 5, 3, true).explode(4, 9) == false);
+}
 /*
 int xbomb;
     cin >> xbomb;

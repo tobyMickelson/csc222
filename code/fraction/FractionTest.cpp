d2616d63d484742441b69dc9a9d0c494533eca40
Toby Mickelson <tmickelson@tinyfern.com>
2024-12-11 08:49:44 -0500

    added test

--- code/fraction/FractionTest.cpp
+++ code/fraction/FractionTest.cpp
@@ -44,3 +44,10 @@ TEST_CASE("Test gcd function") {
     CHECK(gcd(16, 12) == 4);
     CHECK(gcd(18, 27) == 9);
 }
+
+TEST_CASE("Test integer Fractions render properly") {
+    Fraction f1(5, 1);
+    CHECK(f1.to_string() == "5");
+    Fraction f2(18, 3);
+    CHECK(f1.to_string() == "6");
+}
